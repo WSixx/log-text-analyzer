0c974189b3ab50be08e8e6d9d471360f54398f9e
Lucas G <lucas.2007s@gmail.com>
2026-09-06 17:44:35 -0300

    Ui Compose update

--- app/src/main/java/br/com/lucad/logtextanalyzer/MainActivity.kt
+++ app/src/main/java/br/com/lucad/logtextanalyzer/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.lucad.logtextanalyzer.ui.theme.LogTextAnalyzerTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,11 +36,58 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LogTextAnalyzerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                MainApp(Modifier.fillMaxSize())
+            }
+        }
+    }
+}
+
+@Composable
+fun MainApp(modifier: Modifier = Modifier) {
+    Scaffold(modifier = modifier.fillMaxSize()) { innerPadding ->
+        LogAnalyzerContent(
+            onAnalyzeClick = {},
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        )
+    }
+}
+
+@Composable
+fun LogAnalyzerContent(onAnalyzeClick: () -> Unit, modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.background,
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+
+        ) {
+            ElevatedCard(
+                modifier = Modifier.fillMaxWidth(),
+                colors = CardDefaults.elevatedCardColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(24.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    TextHeader(modifier = Modifier.padding(bottom = 20.dp))
+                    ElevatedButton(
+                        onClick = onAnalyzeClick,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(text = "Analyze")
+                    }
                 }
             }
         }
@@ -38,17 +95,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun TextHeader(modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Type Text",
+        style = MaterialTheme.typography.headlineMedium,
         modifier = modifier
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    LogTextAnalyzerTheme {
-        Greeting("Android")
+fun MainAppPreview() {
+    LogTextAnalyzerTheme(dynamicColor = false) {
+        MainApp()
     }
 }
--- nativeanalyzer/src/main/cpp/nativeanalyzer.c
+++ nativeanalyzer/src/main/cpp/nativeanalyzer.c
@@ -1,10 +1,10 @@
 #include <jni.h>
-#include <string>
 
-extern "C" JNIEXPORT jstring JNICALL
+JNIEXPORT jstring JNICALL
 Java_br_com_lucad_nativeanalyzer_NativeLib_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+        JNIEnv *env,
+        jobject thiz) {
+    (void)thiz;
+    const char *hello = "Hello from C";
+    return (*env)->NewStringUTF(env, hello);
 }
